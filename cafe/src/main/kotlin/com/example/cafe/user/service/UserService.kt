2f400755c6fec52b262011e65d14a5d85b7dbf80
DoohyunHwang97 <ghkdengus97@gmail.com>
2024-01-17 00:04:28 +0900

    fix: 유저 엔티티 변경으로 인한 오류 수정

--- cafe/src/main/kotlin/com/example/cafe/user/service/UserService.kt
+++ cafe/src/main/kotlin/com/example/cafe/user/service/UserService.kt
@@ -1,5 +1,7 @@
 package com.example.cafe.user.service
 
+import java.time.LocalDateTime
+
 interface UserService {
-    fun signUp(userId: String, username: String, password: String, email: String, birthDate: String, phoneNumber: String): User
+    fun signUp(userId: String, username: String, password: String, email: String, birthDate: String, phoneNumber: String, at: LocalDateTime = LocalDateTime.now()): User
 }
