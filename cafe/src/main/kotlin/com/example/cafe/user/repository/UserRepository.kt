75c6838bf2f4ed7a0115617832ab803759c0aaf8
chanyoung <vitacpark@snu.ac.kr>
2024-01-28 15:32:53 +0900

    articlesCount increment, decrement feature implemented

--- cafe/src/main/kotlin/com/example/cafe/user/repository/UserRepository.kt
+++ cafe/src/main/kotlin/com/example/cafe/user/repository/UserRepository.kt
@@ -1,8 +1,26 @@
 package com.example.cafe.user.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.transaction.annotation.Transactional
 
 interface UserRepository : JpaRepository<UserEntity, Long> {
     fun findByUsername(username: String): UserEntity?
     fun findByNickname(nickname: String): UserEntity?
+    @Modifying
+    @Transactional
+    @Query("""
+        UPDATE users u SET u.articlesCount = u.articlesCount + 1 WHERE u.id = :userId
+    """)
+    fun incrementArticleCount(userId: Long)
+    @Modifying
+    @Transactional
+    @Query(
+        """
+        UPDATE users u SET u.articlesCount = u.articlesCount - 1 WHERE u.id = :userId
+    """
+    )
+    fun decrementArticleCount(userId: Long)
+
 }
