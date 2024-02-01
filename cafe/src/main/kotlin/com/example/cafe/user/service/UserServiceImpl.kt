706195ee0d99031153d649e04f947fddedea647c
DoohyunHwang97 <ghkdengus97@gmail.com>
2024-02-01 16:46:23 +0900

    feat: email 기입을 선택할 수 있게 변경

--- cafe/src/main/kotlin/com/example/cafe/user/service/UserServiceImpl.kt
+++ cafe/src/main/kotlin/com/example/cafe/user/service/UserServiceImpl.kt
@@ -36,7 +36,7 @@ class UserServiceImpl (
         username: String,
         password: String,
         name: String,
-        email: String,
+        email: String?,
         birthDate: String,
         phoneNumber: String,
         at: LocalDate
@@ -211,7 +211,7 @@ class UserServiceImpl (
         }
     }
 
-    private fun validate(username: String, password: String, email: String, birthDate: String, phoneNumber: String) {
+    private fun validate(username: String, password: String, email: String?, birthDate: String, phoneNumber: String) {
         val validationUtil = ValidationUtil()
 
         if (!validationUtil.isUsernameValid(username)) {
@@ -222,7 +222,7 @@ class UserServiceImpl (
             throw SignUpBadPasswordException()
         }
 
-        if (!validationUtil.isEmailValid(email)) {
+        if (!email.isNullOrEmpty() && !validationUtil.isEmailValid(email)) {
             throw SignUpBadEmailException()
         }
 
